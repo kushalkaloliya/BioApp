d10962664687f7b03708728d95a7a9b5a22bc56c
kushal <kushal.kaloliya@yahoo.com>
2024-12-29 00:48:23 -0500

    First Commit For HTML

---
+++ JavaScript/exercise2.js
@@ -0,0 +1,8 @@
+function calNumbers()
+{
+    const no1=parseFloat(document.getElementById("no_1").value);
+    const no2=parseFloat(document.getElementById("no_2").value);
+    const sum=no1+no2;
+
+    alert(no1+"+"+no2+"="+sum)
+}
